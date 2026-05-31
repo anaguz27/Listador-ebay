834c8962bd85d5fbb9f3bd762bf7fe37b68c21d1
anaguz27 <anaguz27.en@gmail.com>
2026-05-30 17:53:21 -0700

    production oauth

--- api/ebay-auth.js
+++ api/ebay-auth.js
@@ -1,12 +1,6 @@
-export default function handler(req, res) {
-  const APP_ID = process.env.EBAY_APP_ID;
-  const RU_NAME = process.env.EBAY_RU_NAME;
-
-  if (!APP_ID || !RU_NAME) {
-    return res.status(500).json({
-      error: 'Faltan variables EBAY_APP_ID o EBAY_RU_NAME en Vercel.'
-    });
-  }
+export default async function handler(req, res) {
+  const appId = process.env.EBAY_APP_ID;
+  const ruName = process.env.EBAY_RU_NAME;
 
   const scopes = [
     'https://api.ebay.com/oauth/api_scope',
@@ -15,12 +9,12 @@ export default function handler(req, res) {
     'https://api.ebay.com/oauth/api_scope/sell.fulfillment'
   ].join(' ');
 
-  const authUrl =
-    'https://auth.sandbox.ebay.com/oauth2/authorize' +
-    '?client_id=' + encodeURIComponent(APP_ID) +
-    '&response_type=code' +
-    '&redirect_uri=' + encodeURIComponent(RU_NAME) +
-    '&scope=' + encodeURIComponent(scopes);
+  const authUrl = 'https://auth.ebay.com/oauth2/authorize'
+    + '?client_id=' + encodeURIComponent(appId)
+    + '&response_type=code'
+    + '&redirect_uri=' + encodeURIComponent(ruName)
+    + '&scope=' + encodeURIComponent(scopes);
 
-  res.redirect(302, authUrl);
+  res.writeHead(302, { Location: authUrl });
+  res.end();
 }
--- api/ebay-callback.js
+++ api/ebay-callback.js
@@ -1,14 +1,45 @@
 export default async function handler(req, res) {
-  const id = process.env.EBAY_APP_ID, cert = process.env.EBAY_CERT_ID, ru = process.env.EBAY_RU_NAME;
-  const code = req.query.code;
-  if (!code) return res.status(400).send("Falta code");
-  const auth = Buffer.from(id + ":" + cert).toString("base64");
-  const r = await fetch("https://api.sandbox.ebay.com/identity/v1/oauth2/token", {
-    method: "POST",
-    headers: { "Content-Type": "application/x-www-form-urlencoded", "Authorization": "Basic " + auth },
-    body: "grant_type=authorization_code&code=" + encodeURIComponent(code) + "&redirect_uri=" + encodeURIComponent(ru)
-  });
-  const data = await r.json();
-  res.setHeader("Content-Type", "text/plain");
-  res.status(r.ok ? 200 : 400).send(JSON.stringify(data, null, 2));
+  const { code } = req.query;
+  if (!code) {
+    res.status(400).send('Falta el code en la URL');
+    return;
+  }
+
+  const appId = process.env.EBAY_APP_ID;
+  const certId = process.env.EBAY_CERT_ID;
+  const ruName = process.env.EBAY_RU_NAME;
+
+  const credentials = Buffer.from(appId + ':' + certId).toString('base64');
+
+  try {
+    const resp = await fetch('https://api.ebay.com/identity/v1/oauth2/token', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'Authorization': 'Basic ' + credentials
+      },
+      body: new URLSearchParams({
+        grant_type: 'authorization_code',
+        code: decodeURIComponent(code),
+        redirect_uri: ruName
+      })
+    });
+
+    const data = await resp.json();
+
+    if (data.refresh_token) {
+      res.setHeader('Content-Type', 'text/html; charset=utf-8');
+      res.status(200).send(
+        '<h2>OK Refresh token de PRODUCCION</h2>' +
+        '<p>Copialo y guardalo en Vercel como <b>EBAY_REFRESH_TOKEN</b>:</p>' +
+        '<textarea style="width:100%;height:120px;font-size:14px">' +
+        data.refresh_token + '</textarea>' +
+        '<p>Expira en: ' + (data.refresh_token_expires_in / 86400).toFixed(0) + ' dias</p>'
+      );
+    } else {
+      res.status(400).send('<h2>Error</h2><pre>' + JSON.stringify(data, null, 2) + '</pre>');
+    }
+  } catch (err) {
+    res.status(500).send('Error: ' + err.message);
+  }
 }
